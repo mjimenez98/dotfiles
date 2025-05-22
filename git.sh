18d9129b40488ee9ad1fcb5c2a13b6fbe3c722b7
Miguel Jimenez <mjimenez98@github.com>
2025-05-21 21:45:23 -0400

    record git preferences

---
+++ git.sh
@@ -0,0 +1,8 @@
+# Configure name
+git config --global user.name "Miguel Jimenez"
+
+# Merge on pull
+git config --global pull.rebase false
+
+# Convert CRLF to LF on commit
+git config --global core.autocrlf input
